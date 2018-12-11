30c9801bd36db4698c59bbc1ff22c1f47518fdfc
wluka85 <wluka85@gmail.com>
2018-12-11 21:20:00 +0100

    Add selects of body and model to header and create tests to it

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header';
-import {configure, shallow} from "enzyme";
+import {configure, mount, shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
@@ -20,5 +20,21 @@ describe('Header component', () => {
 
     it('has two select inputs', () => {
         expect(wrapper.find('select').length).toEqual(2);
-    })
+    });
+
+    it('has options of bodyList', () => {
+        const bodyList = ['body-1', 'body-2'];
+        const headerWithBodyCar = shallow(<Header bodyList={bodyList} />);
+        bodyList.forEach((body) => {
+            expect(headerWithBodyCar.containsMatchingElement(<option value={body}>{body}</option>)).toEqual(true)
+        });
+    });
+
+    it('has options of modelList', () => {
+        const modelList = ['model-1', 'model-2'];
+        const headerWithModelCar = shallow(<Header modelList={modelList} />);
+        modelList.forEach((model) => {
+            expect(headerWithModelCar.containsMatchingElement(<option value={model}>{model}</option>)).toEqual(true)
+        });
+    });
 });
