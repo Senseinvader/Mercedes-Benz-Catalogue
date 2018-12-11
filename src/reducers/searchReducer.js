9dea473c6c0ab29555f3776d6af3acd01262612d
wluka85 <wluka85@gmail.com>
2018-12-11 23:04:20 +0100

    Implement case in searchReducer FETCH_MODEL_LIST

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -7,6 +7,9 @@ const searchReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_BODY_LIST':
             return {...state, bodyList: action.bodyList};
+
+        case 'FETCH_MODEL_LIST':
+            return {...state, modelList: action.modelList};
         default:
             return state;
     }
