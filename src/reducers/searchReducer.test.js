ae4cd06cd97f102d1d2beb4dc3aa0dd48f8f6038
wluka85 <wluka85@gmail.com>
2018-12-11 23:03:05 +0100

    Add test of fetch model list

--- src/reducers/searchReducer.test.js
+++ src/reducers/searchReducer.test.js
@@ -19,4 +19,15 @@ describe('tests of search reducer', () => {
             bodyList: ['body-1', "body-2"]
         })
     })
+
+    it('should handle FETCH_MODEL_LIST', () => {
+        expect(
+            searchReducer([], {
+                type: 'FETCH_MODEL_LIST',
+                modelList: ['model-1', 'model-2']
+            })
+        ).toEqual({
+            modelList: ['model-1', "model-2"]
+        })
+    })
 });
