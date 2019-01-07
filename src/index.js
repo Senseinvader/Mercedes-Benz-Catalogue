6ec9ab4897db3c0ea4970f87b297bd5f5ca834bc
wluka85 <wluka85@gmail.com>
2019-01-07 18:45:09 +0100

    Add store to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from './reducers/rootReducer'
 import {Provider} from 'react-redux';
+import thunk from "redux-thunk";
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
