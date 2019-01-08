a6732b82eed6d93f930e854dee10e9f8d86213a3
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-08 14:15:37 +0100

    Refactor - get configurationLoaded flag from props in sideBlock component

--- src/components/sideBlock.js
+++ src/components/sideBlock.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import { stat } from 'fs';
 
 
 export class SideBlock extends Component {
@@ -7,15 +8,15 @@ export class SideBlock extends Component {
     render() {
         const {modelConfiguration, configurationLoaded} = this.props;
         if (configurationLoaded){
-        return(
-            <div className="data-container">
-                <div className='char price'><h3>Price: {modelConfiguration.price} {modelConfiguration.currency}</h3></div>
-                <div className='char power'><h3>Power: {modelConfiguration.power} {modelConfiguration.powerUnit}</h3></div>
-                <div className='char capacity'><h3>Capacity: {modelConfiguration.capacity} {modelConfiguration.capacityUnit}</h3></div>
-                <div className='char speed'><h3>Top speed: {modelConfiguration.topSpeed} {modelConfiguration.topSpeedUnit}</h3></div>
-                <div className='char acceleration'><h3>Acceleration:{modelConfiguration.acceleration} {modelConfiguration.accelerationUnit}</h3></div>
-            </div>
-        );
+            return(
+                <div className="data-container">
+                    <div className='char price'><h3>Price: {modelConfiguration.price} {modelConfiguration.currency}</h3></div>
+                    <div className='char power'><h3>Power: {modelConfiguration.power} {modelConfiguration.powerUnit}</h3></div>
+                    <div className='char capacity'><h3>Capacity: {modelConfiguration.capacity} {modelConfiguration.capacityUnit}</h3></div>
+                    <div className='char speed'><h3>Top speed: {modelConfiguration.topSpeed} {modelConfiguration.topSpeedUnit}</h3></div>
+                    <div className='char acceleration'><h3>Acceleration:{modelConfiguration.acceleration} {modelConfiguration.accelerationUnit}</h3></div>
+                </div>
+            );
         } else {
             return <div></div>;
         }
@@ -25,6 +26,7 @@ export class SideBlock extends Component {
 export const mapStateToProps = (state) => {
     return {
         modelConfiguration: state.headerReducer.modelConfiguration,
+        configurationLoaded: state.headerReducer.configurationLoaded
     }
 };
 
