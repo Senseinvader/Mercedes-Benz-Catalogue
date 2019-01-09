a0e47436704b1e1d27bd7423bf77d1e7890c7ea9
wluka85 <wluka85@gmail.com>
2019-01-09 15:00:51 +0100

    Implement description-actions and description reducer

---
+++ src/actions/description-actions.js
@@ -0,0 +1,31 @@
+const setDescriptionId = (description, descriptionId) => ({
+    type: 'SET_DESCRIPTION',
+    description: description,
+    descriptionId: descriptionId
+});
+
+export const incremetDescriptionID = () => (dispatch, getState) => {
+    const descriptionList = getState().headerReducer.modelConfiguration.vehicleComponents;
+    console.log(descriptionList)
+    const listLength = descriptionList.length;
+    let descriptionId = getState().descriptionReducer.descriptionId;
+    descriptionId++;
+    if (descriptionId >= listLength) {
+        descriptionId = 0;
+    }
+    const description = descriptionList[descriptionId].name;
+    dispatch(setDescriptionId(description, descriptionId));
+};
+
+export const decremetDescriptionID = () => (dispatch, getState) => {
+    const descriptionList = getState().headerReducer.modelConfiguration.vehicleComponents;
+    const listLength = descriptionList.length;
+    let descriptionId = getState().descriptionReducer.descriptionId;
+    descriptionId--;
+    if (descriptionId <= 0) {
+        descriptionId = listLength -1;
+    }
+    const description = descriptionList[descriptionId].name;
+    dispatch(setDescriptionId(description, descriptionId));
+};
+
