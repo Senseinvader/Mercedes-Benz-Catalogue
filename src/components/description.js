609b2c0c6a8b29b63de4495d85e63a15a2a919da
wluka85 <wluka85@gmail.com>
2019-01-09 14:56:03 +0100

    Add description of car components

--- src/components/description.js
+++ src/components/description.js
@@ -1,28 +1,51 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import {decremetDescriptionID, incremetDescriptionID} from "../actions/description-actions";
 
 export class Description extends Component {
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const {description, changeToNextDescription} = this.props;
+        if (description === '') {
+            changeToNextDescription();
+        }
+    }
+
     render() {
-        const {exterior, configurationLoaded} = this.props;
+        const {configurationLoaded, description, changeToNextDescription, changeToPreviousDescription} = this.props;
+
         if (configurationLoaded) {
             return (
-                <div className='description'>
-                    <h3>
-                        {exterior ? "This is exterior photo" : "This is interior photo"}
-                    </h3>
+                <div className='description-wrapper'>
+                    <a className="waves-effect waves-light btn-large" onClick={changeToPreviousDescription}>&lt;</a>
+                    <div>{description}</div>
+                    <a className="waves-effect waves-light btn-large" onClick={changeToNextDescription}>&gt;</a>
                 </div>
             );
         } else {
             return <div></div>;
         }
     }
+
+
 }
 
 export const mapStateToProps = (state) => {
     return {
+        description: state.descriptionReducer.description,
         exterior: state.sliderReducer.exterior,
         configurationLoaded: state.headerReducer.configurationLoaded
     }
 };
 
-export default connect(mapStateToProps)(Description);
+export const mapDispatchToProps = (dispatch) => {
+    return {
+        changeToNextDescription: () => { dispatch(incremetDescriptionID()) },
+        changeToPreviousDescription: () => { dispatch(decremetDescriptionID()) }
+
+    }
+
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Description);
