f3a77889284861e640c6671ace93f8322cbb04e1
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-09 10:15:39 +0100

    Style the sideBlock component

--- src/components/sideBlock.js
+++ src/components/sideBlock.js
@@ -8,12 +8,16 @@ export class SideBlock extends Component {
         const {modelConfiguration, configurationLoaded} = this.props;
         if (configurationLoaded){
             return(
-                <div className="data-container">
-                    <div className='char price'><p>Price: {modelConfiguration.price} {modelConfiguration.currency}</p></div>
-                    <div className='char power'><p>Power: {modelConfiguration.power} {modelConfiguration.powerUnit}</p></div>
-                    <div className='char capacity'><p>Capacity: {modelConfiguration.capacity} {modelConfiguration.capacityUnit}</p></div>
-                    <div className='char speed'><p>Top speed: {modelConfiguration.topSpeed} {modelConfiguration.topSpeedUnit}</p></div>
-                    <div className='char acceleration'><p>Acceleration:{modelConfiguration.acceleration} {modelConfiguration.accelerationUnit}</p></div>
+                <div className="data-container row">
+                    <div className='col s12'>
+                        <div className='card-panel teal'>
+                            <div className='char price white-text'><p>Price: {modelConfiguration.price} {modelConfiguration.currency}</p></div>
+                            <div className='char power white-text'><p>Power: {modelConfiguration.power} {modelConfiguration.powerUnit}</p></div>
+                            <div className='char capacity white-text'><p>Capacity: {modelConfiguration.capacity} {modelConfiguration.capacityUnit}</p></div>
+                            <div className='char speed white-text'><p>Top speed: {modelConfiguration.topSpeed} {modelConfiguration.topSpeedUnit}</p></div>
+                            <div className='char acceleration white-text'><p>Acceleration:{modelConfiguration.acceleration} {modelConfiguration.accelerationUnit}</p></div>
+                        </div>
+                    </div>
                 </div>
             );
         } else {
