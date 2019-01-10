ad220e3eb56fa5b58383b8df6d5c7ff28721fceb
wluka85 <wluka85@gmail.com>
2019-01-10 22:35:24 +0100

    Repair test (should show sexterior of the initial value)

--- src/components/slider.test.js
+++ src/components/slider.test.js
@@ -4,6 +4,7 @@ import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 import {changeImage} from "../actions/sliderActions";
+import CarModel from "../model/carModel";
 
 configure({adapter: new Adapter()});
 
@@ -12,8 +13,12 @@ describe('Slider component', () => {
     //test mapStateToProps
     it('should show sexterior of the initial value', () => {
         const initialState = {
-            exterior: true,
-            imagesArray: []
+            sliderReducer: {
+                exterior: true
+            },
+            headerReducer: {
+                modelConfiguration: new CarModel()
+            },
         };
         expect(mapStateToProps(initialState).exterior).toBe(true);
     });
