943000fa0cae829bd51bf0bd731ab8a934c836d0
wluka85 <wluka85@gmail.com>
2019-01-11 00:26:57 +0100

    Refactor in code

--- src/components/sideBlock.js
+++ src/components/sideBlock.js
@@ -5,7 +5,6 @@ export class SideBlock extends Component {
 
     render() {
         const {modelConfiguration, configurationLoaded} = this.props;
-        console.log('configuartion', modelConfiguration);
         if (configurationLoaded){
             return(
                 <div className="data-container">
