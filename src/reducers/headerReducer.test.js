ccba8c9bca2259dfcf706c54171449e064741750
wluka85 <wluka85@gmail.com>
2019-01-10 23:57:21 +0100

    Repair headerReducer test

--- src/reducers/headerReducer.test.js
+++ src/reducers/headerReducer.test.js
@@ -5,8 +5,11 @@ describe('tests of search reducer', () => {
 
     it('should return the initial state', () => {
         expect(headerReducer(undefined, {})).toEqual({
-                bodyList: [{bodyId: '', bodyName: 'Empty'}],
-                modelList: [{modelId: '', modelName: 'Empty'}],
+                bodyList: [{bodyId: '', bodyName: 'Loading...'}],
+                bodyLoaded: true,
+                modelList: [{modelId: '', modelName: 'Loading...'}],
+                modelLoaded: true,
+                picturesURL: undefined,
                 loading: false,
                 error: null,
                 modelConfiguration: new CarModel(),
@@ -20,6 +23,7 @@ describe('tests of search reducer', () => {
                 type: 'FETCH_BODIES_REQUEST'
             })
         ).toEqual({
+            bodyLoaded: true,
             loading: true,
             modelConfiguration: new CarModel()
         });
@@ -37,7 +41,8 @@ describe('tests of search reducer', () => {
 
         ).toEqual({
                 bodyList: bodyList,
-                loading:false
+                loading:false,
+            bodyLoaded: false
         });
     });
 
@@ -61,7 +66,8 @@ describe('tests of search reducer', () => {
                type: 'FETCH_MODELS_REQUEST'
         })
        ).toEqual({
-            loading: true
+            loading: true,
+            modelLoaded: true
         })
     });
 
@@ -75,7 +81,8 @@ describe('tests of search reducer', () => {
             })
         ).toEqual({
             modelList,
-            loading: false
+            loading: false,
+            modelLoaded: false
         })
     });
 
