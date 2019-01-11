2196b8ca606546dbbab3caf88607d490c1800fe8
wluka85 <wluka85@gmail.com>
2019-01-11 09:51:34 +0100

    Change api-key and delete tryout in queries

--- src/actions/apiQueries.js
+++ src/actions/apiQueries.js
@@ -1,10 +1,10 @@
 import {API_KEY} from "../non-export-data/api-key";
 
-export const getBodiesQuery = `https://api.mercedes-benz.com/configurator_tryout/v1/markets/de_DE/bodies?apikey=${API_KEY}`;
+export const getBodiesQuery = `https://api.mercedes-benz.com/configurator/v1/markets/de_DE/bodies?apikey=${API_KEY}`;
 export const getModelsQuery = (bodyId) => {
-    return `https://api.mercedes-benz.com/configurator_tryout/v1/markets/de_DE/models?bodyId=${bodyId}&apikey=${API_KEY}`;
+    return `https://api.mercedes-benz.com/configurator/v1/markets/de_DE/models?bodyId=${bodyId}&apikey=${API_KEY}`;
 };
 
 export const getModelConfigurationQuery = (modelId) => {
-    return `https://api.mercedes-benz.com/configurator_tryout/v1/markets/de_DE/models/${modelId}/configurations/initial?apikey=${API_KEY}`
+    return `https://api.mercedes-benz.com/configurator/v1/markets/de_DE/models/${modelId}/configurations/initial?apikey=${API_KEY}`
 };
