9167da1777ffdd34890e5c8028e62a24544992e5
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-18 16:31:05 +0100

    Update react to 16.7, add ReduxDevTools to project by composing it with store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import rootReducer from './reducers/rootReducer'
 import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
 
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
